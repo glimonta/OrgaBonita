176d0f18e9389db0c4b2a687e51884d2d1aacfc1
glimonta <glimonta@gmail.com>
2012-10-27 12:04:07 -0430

    Lo logra con puntuaciones de un digito. Prueba cambiando la puntuacion dentro de HighScore.s para que veas :')

--- Proyecto1/HighScore.s
+++ Proyecto1/HighScore.s
@@ -23,7 +23,7 @@
 .data
 
 nomArch:        .asciiz "score.txt"
-nuevPunt:       .asciiz "7 Sant"
+nuevPunt:       .asciiz "9 Sant"
 prim:           .space 10
 seg:            .space 10
 ter:            .space 10
@@ -56,6 +56,8 @@ leer1:  move $a0, $t0
 
         lb $t4, 0($a0)
         sb $t4, 0($t1)
+        
+        beq $t4, 0xa, inter2
 
         addi $t1, $t1, 1
         
@@ -78,6 +80,8 @@ leer2:  move $a0, $t0
         lb $t4, 0($a0)
         sb $t4, 0($t1)
         
+        beq $t4, 0xa, inter3
+        
         addi $t1, $t1, 1
         
         b leer2
@@ -99,44 +103,30 @@ leer3:  move $a0, $t0
         lb $t4, 0($a0)
         sb $t4, 0($t1)
         
+        beq $t4, 0xa, leer3
+        
         addi $t1, $t1, 1
 	
         b leer3
         
 comparar:       lb $t5, nuevPunt        #cargo el nuevo puntaje en t5
-
-                move $s0, $zero
                 
-                la $t1, prim            #cargo prim en t1
-                lb $t4, 0($t1)                    #cargo prim en t4
-                bgt $t5, $t4, escrib1a    #si la nueva puntuacion es mas
-                                                #grande sobreescribo
+                la $t1, prim         
+                lb $t4, 0($t1)                    
+                bgt $t5, $t4, escrib1a 
                 
-                la $t1, seg             #cargo seg en t2
-                move $a0 $t1
-                li $v0 4
-                syscall
+                la $t1, seg    
                 lb $t4, 0($t1)     
                 bgt $t5, $t4, escrib2a
-
-                la $t3, ter             #cargo ter en t3
-                lb $t4, 0($t3)
-                blt $t5, $t4, escrib3a
-          
-escrib1a:       la $t5, seg
-                la $t4, ter
-                b sobreescribir1
-
-escrib1b:       move $s0, $zero
-                la $t5, prim
-                la $t4, seg
-                b sobreescribir1
-
-escrib1c:       move $s0, $zero
-                la $t5, nuevPunt
-                la $t4, prim
                 
-sobreescribir1: lb $t6, 0($t5)
+                la $t3, ter            
+                lb $t4, 0($t3)
+                blt $t5, $t4, escrib3
+          
+escrib3:        la $t5, nuevPunt
+                la $t4, ter
+                
+sobreescribir3: lb $t6, 0($t5)
 
                 move $a0, $t6
                 li $v0, 11
@@ -146,13 +136,12 @@ sobreescribir1: lb $t6, 0($t5)
 
                 addi $t5, $t5, 1
                 addi $t4, $t4, 1
-                addi $s0, $s0, 1
 
-                bnez $t6, sobreescribir1
+                bnez $t6, sobreescribir3
                 b fin
 
-escrib2:        la $t5, nuevPunt
-                la $t4, seg
+escrib2a:       la $t5, seg
+                la $t4, ter
 
 sobreescribir2: lb $t6, 0($t5)
 	
@@ -166,12 +155,28 @@ sobreescribir2: lb $t6, 0($t5)
                 addi $t4, $t4, 1
 
                 bnez $t6, sobreescribir2
+
+escrib2b:       la $t5, nuevPunt
+                la $t4, seg
+                
+sobreescribir2b: lb $t6, 0($t5)
+        
+                move $a0, $t6
+                li $v0, 11
+                syscall
+
+                sb $t6, 0($t4)
+
+                addi $t5, $t5, 1
+                addi $t4, $t4, 1
+
+                bnez $t6, sobreescribir2b
                 b fin
 
-escrib3: la $t5, nuevPunt
+escrib1a:       la $t5, seg
                 la $t4, ter
 
-sobreescribir3: lb $t6, 0($t5)
+sobreescribir1: lb $t6, 0($t5)
 
                 move $a0, $t6
                 li $v0, 11
@@ -182,7 +187,39 @@ sobreescribir3: lb $t6, 0($t5)
                 addi $t5, $t5, 1
                 addi $t4, $t4, 1
                 
-                bnez $t6, sobreescribir3
+                bnez $t6, sobreescribir1
+                
+escrib1b:       la $t5, prim
+                la $t4, seg
+                
+sobreescribir1b: lb $t6, 0($t5)
+
+                move $a0, $t6
+                li $v0, 11
+                syscall
+
+                sb $t6, 0($t4)
+
+                addi $t5, $t5, 1
+                addi $t4, $t4, 1
+                
+                bnez $t6, sobreescribir1b
+                
+escrib1c:       la $t5, nuevPunt
+                la $t4, prim
+                
+sobreescribir1c: lb $t6, 0($t5)
+
+                move $a0, $t6
+                li $v0, 11
+                syscall
+
+                sb $t6, 0($t4)
+
+                addi $t5, $t5, 1
+                addi $t4, $t4, 1
+                
+                bnez $t6, sobreescribir1c
 
 fin:    li $v0, 10
         syscall
