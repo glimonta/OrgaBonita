024abeb0e53c07a41af54943b4864c210d9433be
Luis Miranda <lcmm1993@gmail.com>
2012-11-26 21:34:49 -0500

    comentado

--- Proyecto2/exceptionsEndogeno.s
+++ Proyecto2/exceptionsEndogeno.s
@@ -19,6 +19,11 @@ dummy:	.ascii "a "
 	
 	.ktext 0x80000180
 
+#
+# Estaba en el ejemplo
+# esta guardando los supuestos registros importantes
+#
+	
 	.set noat
 	move $k1 $at		# Save $at
 	.set at
@@ -31,7 +36,12 @@ dummy:	.ascii "a "
 	andi $a0 $a0 0x1f
 
 ###################################################################
-	
+#
+# Aqui se carga el vector de la interrupcion de teclado y se revisa a
+# ver si fue una interrupcion de teclado
+# si es de teclado el resultado no deberia ser 0
+# si es = 0 entonces entro por interrupcion de timer y se va a display
+#
 	lui $t0 0xFFFF
 
 	lw $t1, 0($t0) 
@@ -39,7 +49,10 @@ dummy:	.ascii "a "
 	beq $t1, $zero, display
 
 ##################################################################
-
+#
+# Una vez que es una interrupcion de teclado revisas si es "q"
+# true = me salgo
+#
 	addi $s0, $0, 113 # q
 
 	lw $a1, 4($t0)
@@ -48,6 +61,12 @@ dummy:	.ascii "a "
 	li $v0 10
 	syscall
 
+#
+# Como en este caso si no es q entonces deberia ser de movimiento
+# entramos en el procedimiento que dependiendo de lo que lea enciende
+# un bit en direccion y retorna al ciclo infinito
+#
+	
 Mov:	lw $k0 direccion
 
 	li $v0,4
@@ -68,6 +87,7 @@ Mov:	lw $k0 direccion
 # 0010 = down
 # 0100 = left
 # 1000 = rigth
+	
 mU:	ori $k0 0x0001
 	b print
 mD:	ori $k0 0x0002
@@ -76,18 +96,22 @@ mL:	ori $k0 0x0004
 	b print
 mR:	ori $k0 0x0008
 
-#
-# AQUI HAY UN ERROR QUE TENGO QUE REVISAR
-# funciona sin el 0x0010	
-print:	#ori $k0 0x0010
-	sw $k0 direccion
+print:	sw $k0 direccion
 
-	
 	b end
 
 ##################################################################
-	
-display:lw $a0 direccion
+#
+# Si entro por timer entonces tiene que actualizar el monitor
+#
+#
+display:
+
+#
+# Guardo todo lo que tengo que guardar y paso lo que tenga que pasar
+# como parametro para poder llamar a la funcion que mueve al pacman
+#
+	lw $a0 direccion
 	move $a1 $t5
 	la $a2 m1
 	lw $a3 tamano
@@ -107,7 +131,10 @@ display:lw $a0 direccion
 	lw $v0 16($sp)
 	addi $sp $sp 16
 	addi $fp $sp 16
-	
+
+#
+# Imprimo el tablero con 4 lineas
+# 
 	
 	li $v0,4
 	la $a0, m1
@@ -117,6 +144,8 @@ display:lw $a0 direccion
 	la $a0, new_line
 	syscall
 
+# Reinicio el timer
+	
 	li $t0, 5
 	mtc0 $t0, $11
 	mtc0 $zero, $9
@@ -141,6 +170,8 @@ end:	lw $v0 s1		# Restore other registers
 
 ######################################################
 #
+# Funcion que lo mueve
+#
 # a1 = la posicion de lo que vas a mover
 # a0 = dirrecion
 # a2 = mapa
@@ -148,7 +179,6 @@ end:	lw $v0 s1		# Restore other registers
 # t0 = 0(dummy)
 # t2 = dummy
 # t3 = destino (arriba y abajo)
-# t4 = 1 pacman 0 fantasma
 # t5 = x
 # t1 = lo que esta en destino
 #
@@ -156,7 +186,7 @@ end:	lw $v0 s1		# Restore other registers
 mover:
 	li $t0 0
 	lb $t0 0($a1)
-	lb $t4 1($a1)
+
 	sb $t0 dummy
 
 	la $t2 dummy
@@ -165,6 +195,7 @@ mover:
 	# deberia ir $fp en vez de $sp
 	lw $v0, 12($sp)
 
+# reviso que bit tengo prendido
 	li $t0, 8
 	beq $a0, $t0, der
 	li $t0, 4
@@ -173,11 +204,18 @@ mover:
 	beq $a0, $t0, baj
 	li $t0, 1
 	beq $a0, $t0, arr
-	
+
+# Si no tengo ninguno de esos pendidos
+# limpio direccion y me salgo
 	li $a0 0
 	sw $a0 direccion
 	j $ra
 
+#
+# Para moverme hacia arriba como tengo el tama~o
+# de cada fila de la matriz lo que restar esa cantidad y
+# asi salto a la posicion que esta "arriba"
+#
 arr:
 	nor $a3 $a3 $a3
 	addi $a3 $a3 1
@@ -188,18 +226,28 @@ arr:
 
 	li $t5 0x78	
 	beq $t1 $t5 k
-	
+
 	li $t0 0x56
 	li $t6 0x6F
 
+	# en dummy guardo lo que estaba a donde me voy a mover
 	sb $t1 0($t2)
+	# cargo "V" en la posicion a donde me voy a mover
 	sb $t0 0($t3)
+	# coloco "o" donde estaba antes
 	sb $t6 0($a1)	
 
+	#devuelvo la nueva posicion del pacman
 	move $v0 $t3 
 
 	b k
-	
+
+#
+# Lo mismo que arriba solo que se suma la cantidad para
+# asi bajar una en vez de subir
+#
+# Esta falta ponerla bonita como la de arriba para fines practicos
+# hace lo mismo que arriba
 baj:
 
 	lb $t0 0($t2)
@@ -226,7 +274,9 @@ bajo:	sb $t1 0($t2)
 	li $t0 0x6F
 	sb $t0 0($a1)
 	b k
-
+#
+# +1 para decir que es a la derecha
+#
 der:
 
 	lb $t0 0($t2)
@@ -245,6 +295,10 @@ der:
 	li $t0 0x6F
 	sb $t0 0($a1)
 	b k
+
+#
+# -1 para decir que es a la izquirda
+#
 
 izq:
 
@@ -265,18 +319,27 @@ izq:
 k:	li $a0 0
 	sw $a0 direccion
  	j $ra
-#####################################################
 	
+#####################################################
+#
+#
+# Aqui comienza el programa, cuando comienza el carga esto
+#
+#######################################################
 	.text
 	.globl __start
 
 __start:
+# Esto estaba en el ejemplo que ellos dieron no se que es
+	
 	lw $a0 0($sp)		# argc
 	addiu $a1 $sp 4		# argv
 	addiu $a2 $a1 4		# envp
 	sll $v0 $a0 2
 	addu $a2 $a2 $v0
 
+# Esto habilita las interrupciones por teclaso
+	
 	li $a0, 0xffff0000
 	lw $t0, 0($a0)
 	ori $t0, 0x02  # use keyboard interrupts
@@ -285,11 +348,14 @@ __start:
 	mfc0 $t0, $12
 	ori $t0, $t0, 0xff01
 	mtc0 $t0, $12
-	
+
+# Aqui se configura el timer a 5ms
 	li $t0, 5
 	mtc0 $t0, $11
 	mtc0 $zero, $9
 
+# Me carga la posicion del pacman al apuntador $t5
+	
 	la $t5 m1
 	la $t5 24($t5)
 
@@ -299,10 +365,13 @@ __start:
 
 	
 ################################################
-	
+# Esto estaba en el ejemplo asi que lo dejo	
 	jal main
 	nop
-
+	
+# Cuando el main hace j $ra entra en el loop infinito,
+# nada mas sale de ahi si se encuentra con una interrupcion
+	
 loop:	
 	b loop
 
